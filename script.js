44c610b12e8a3e6e022660475f790b1955902c60
Renan Caldas <renan@renancaldas.dev.br>
2023-01-25 11:46:03 -0300

    finalizando carrinho mobile

--- script.js
+++ script.js
@@ -128,7 +128,19 @@ document
     closeModal();
   });
 
+document.querySelector(".menu-openner").addEventListener("click", () => {
+  if (cart.length > 0) {
+    document.querySelector("aside").style.left = 0;
+  }
+});
+
+document.querySelector(".menu-closer").addEventListener("click", () => {
+  document.querySelector("aside").style.left = "100vw";
+});
+
 function updateCart() {
+  document.querySelector(".menu-openner span").innerHTML = cart.length;
+
   if (cart.length > 0) {
     document.querySelector("aside").classList.add("show");
 
@@ -204,5 +216,6 @@ function updateCart() {
     ).innerHTML = `R$ ${total.toFixed(2)}`;
   } else {
     document.querySelector("aside").classList.remove("show");
+    document.querySelector("aside").style.left = "100vw";
   }
 }
